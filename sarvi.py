5b12efde2d920b6cac00167717ede5ff555f65ca
Mehran <mehran1414@gmail.com>
2020-07-21 00:57:56 +0430

    *This is a big one :) -I install a package named "dython" to see the categorical correlations. -A figure added to the project resulting from the above. -I used Quantile method to omit some of the outliers based on 0.01 and 0.99 thresholds.

--- sarvi.py
+++ sarvi.py
@@ -51,6 +51,16 @@ plt.figure(figsize=(14,12))
 foo = sns.heatmap(train.corr(), vmax=0.6,square=True, annot=True)
 plt.show()
 
+'''I found a new package! Although in its tutorial showed its performance on
+mixed categorical-numerical datasets, I just get the categorical becuase the 
+the computation is so heavy!
+'''
+from dython.nominal import associations
+train_categorical = train[['site_id','site_domain','site_category',
+                     'app_id', 'app_domain', 'app_category', 'device_id',
+                     'device_ip', 'device_model']]
+associations(train_categorical, theil_u=True, figsize=(30, 30))
+
 '''
 Seems that 'Unnamed: 0', 'click', 'hour', 'C14', 'C17' are highly correlated to click rate.
 In the next section we will plot their inter connections
@@ -69,8 +79,15 @@ sns.jointplot("C14", "click", data=train1, kind="reg",
                   xlim=(0, 60), ylim=(0, 5), color="r", size=7)
 plt.show()
 
-# I will drop the object(string) object and get the dist plot for the remaining variables.
-train1.info()
+
+
+
+
+# =============================================================================
+# I will drop the object(string) object and get the dist plot for the remaining 
+# variables.
+# =============================================================================
+train.info()
 
 # Select the string objects :
 train1 = train.drop(['Unnamed: 0', 'id', 'site_id','site_domain','site_category',
@@ -83,15 +100,62 @@ for col in range(len(train1.columns)+1):
     sns.distplot(train1.iloc[:, int(col)].values, axlabel= col_names[col])
 
 '''
-Unfortunatly the dist plot is not clear. For e.g. the click chart is not clean becasue it is bolean or other features
+Unfortunatly the dist plot is not clear. For e.g. the click chart is not clean
+ becasue it is bolean or other features
 are somehow have limited levels.
 '''
 
-# I am going to check for outliers :
+# =============================================================================
+# I am going to check for outliers : (just sample, feel free to change variables 
+# in your fork!)
+# =============================================================================
+
+#boxplot
+ax = sns.boxplot(x= 'C15', y= 'banner_pos' , hue = 'click'  ,data= train1)
+plt.show()
+
+#scatter plot
+fig, ax = plt.subplots(figsize=(16,8))
+ax.scatter(train1['C16'], train1['C21'])
+ax.set_xlabel('C16')
+ax.set_ylabel('C21')
+plt.show()
+
+#Quantile
+'''
+The 0.25 and 0.75 quantile ommit enormous amount of data, so I used 0.01 and 0.99
+instead. 
+'''
+train1_low = train.quantile(0.01)
+train1_high = train.quantile(0.99)
+IQR = train1_high - train1_low
+
+
+train_out = train[~((train< (train1_low -  IQR)) |
+                               (train > (train1_high +  IQR))).any(axis=1)]
+train_out.shape
+
+'''
+Also ther is another way to do this and it is Z-score. I open this discussion
+ to you!
+'''
+# =============================================================================
+# Helping from an online notebook from Kaggle. I want to see how many unique values
+# are we have in data. This is particularly is the case when I want to use OneHotEncoder
+# or to delete any attribute.
+# =============================================================================
+
+len_of_feature_count = []
+for i in train_out.columns[2:23].tolist():
+    print(i, ':', len(train_out[i].astype(str).value_counts()))
+    len_of_feature_count.append(len(train_out[i].astype(str).value_counts()))
+
+As you may see in this part of the code and previous parts, 
 
 
 
 
 
 
-    # here's the parameters of your distribution, scale, location
+
+
